200385424056de4683a7f2a5fe0800ced469f74c
zhangchenchen <1520406629@qq.com>
2023-04-02 15:18:19 +0800

    commit kafka sink stream

---
+++ big-data-flink/src/main/java/com/e2mg/bigdata/flink/sink/KafkaStream.java
@@ -0,0 +1,42 @@
+package com.e2mg.bigdata.flink.sink;
+
+import org.apache.flink.api.common.serialization.SimpleStringEncoder;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.core.fs.Path;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.filesystem.StreamingFileSink;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+
+import java.util.Properties;
+
+/**
+ * kafka测试
+ *
+ * @author EdiwalMusk
+ * @date 2023/4/2 11:47
+ */
+public class KafkaStream {
+
+    public static void main(String[] args) throws Exception {
+        // 1.创建执行环境
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "192.168.94.194:9092");
+        properties.setProperty("group.id", "flink_group");
+        properties.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.setProperty("auto.offset.reset", "latest");
+
+        DataStreamSource<String> kafkaStream = env.addSource(new FlinkKafkaConsumer<>("clicks",
+                new SimpleStringSchema(), properties));
+
+        StreamingFileSink<String> stringStreamingFileSink = StreamingFileSink.<String>forRowFormat
+                (new Path("./output"), new SimpleStringEncoder<>("UTF-8")).build();
+
+        kafkaStream.addSink(stringStreamingFileSink);
+
+        env.execute();
+    }
+}
